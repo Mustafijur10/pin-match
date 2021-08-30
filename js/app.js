a99c11d0b4bc0fd1a7f2074243e75c57f467f81b
T10vir <15-30734-3@student.aiub.edu>
2021-08-30 22:26:17 +0600

    Input field got numbers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
-function generatePin() {
+
+function getPin() {
     const pin = Math.round(Math.random() * 10000);
-    console.log(pin);
+    const pinString = pin + '';
+
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        console.log('got 3 digit thas why calling again', pin);
+        return getPin();
+    }
 }
+
+function generatePin() {
+
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+
+    calcInput.value = newNumber;
+})
